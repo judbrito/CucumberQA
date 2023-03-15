1445d19b8c06b944af8d8725d64c29cfc0ddf2a8
Judbrito <admoceano@gmail.com>
2023-03-15 16:13:57 -0300

    identificado erro e eliminado

--- src/test/java/Steps/senhorBarriga/SiteBarriga.java
+++ src/test/java/Steps/senhorBarriga/SiteBarriga.java
@@ -1,11 +1,13 @@
 package Steps.senhorBarriga;
 
 import io.cucumber.core.cli.Main;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -75,12 +77,8 @@ public class SiteBarriga {
 	public void aContaÉInseridaComSucesso() {
 		String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
 		Assert.assertEquals("Conta adicionada com sucesso!", texto);
-		driver.findElement(By.linkText("Contas")).click();
-		driver.findElement(By.linkText("Listar")).click();
-	
-		driver.findElement(By.xpath("//span[@class='glyphicon glyphicon-remove-circle']")).click();
-		}
-	
+
+	}
 
 	@Então("sou notificado que o nome da conta é obrigatório")
 	public void souNotificadoQueONomeDaContaÉObrigatório() {
@@ -90,22 +88,25 @@ public class SiteBarriga {
 
 	@Então("sou notificado que já existe uma conta com esse nome")
 	public void souNotificadoQueJáExisteUmaContaComEsseNome() {
+
 		String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
 		Assert.assertEquals("Já existe uma conta com esse nome!", texto);
-		
+
 		driver.findElement(By.linkText("Contas")).click();
-		driver.findElement(By.linkText("Listar")).click();	
+		driver.findElement(By.linkText("Listar")).click();
 		driver.findElement(By.xpath("//span[@class='glyphicon glyphicon-remove-circle']")).click();
 	}
-	
+
 	@Before
 	public void abrir() {
-	System.out.println("começando rodar teste aqui");
-		
+
+		System.out.println("começando rodar teste aqui");
+
 	}
+
 	@After
 	public void fechar() {
-	//	driver.quit();
+		driver.quit();
 		System.out.println("terminou o teste");
 	}
 
--- target/reporthtml/report.js
+++ target/reporthtml/report.js
@@ -4,10 +4,10 @@ formatter.feature({
   "description": "Como um usuário \nGostaria de cadastrar contas\nPara que eu possa distribuir meu dinheiro de uma forma mais organizada",
   "keyword": "Funcionalidade"
 });
-formatter.scenario({
-  "name": "Deve inserir uma conta com sucesso",
+formatter.background({
+  "name": "",
   "description": "",
-  "keyword": "Cenário"
+  "keyword": "Contexto"
 });
 formatter.before({
   "status": "passed"
@@ -82,6 +82,11 @@ formatter.match({
 formatter.result({
   "status": "passed"
 });
+formatter.scenario({
+  "name": "Deve inserir uma conta com sucesso",
+  "description": "",
+  "keyword": "Cenário"
+});
 formatter.step({
   "name": "informo a conta \"Conta de Teste\"",
   "keyword": "E "
@@ -108,6 +113,228 @@ formatter.step({
 });
 formatter.match({
   "location": "SiteBarriga.aContaÉInseridaComSucesso()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Contexto"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "que estou acessando a aplicação",
+  "keyword": "Dado "
+});
+formatter.match({
+  "location": "SiteBarriga.queEstouAcessandoAAplicação()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "informo o usuário \"algumacoisa@gmail.com\"",
+  "keyword": "Quando "
+});
+formatter.match({
+  "location": "SiteBarriga.informoOUsuário(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "a senha \"123456789\"",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.aSenha(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono entrar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoEntrar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "visualizo a página inicial",
+  "keyword": "Então "
+});
+formatter.match({
+  "location": "SiteBarriga.visualizoAPáginaInicial()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono Contas",
+  "keyword": "Quando "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoContas()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono Adicionar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoAdicionar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Não deve inserir uma conta sem nome",
+  "description": "",
+  "keyword": "Cenário"
+});
+formatter.step({
+  "name": "seleciono Salvar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoSalvar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "sou notificado que o nome da conta é obrigatório",
+  "keyword": "Então "
+});
+formatter.match({
+  "location": "SiteBarriga.souNotificadoQueONomeDaContaÉObrigatório()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Contexto"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "que estou acessando a aplicação",
+  "keyword": "Dado "
+});
+formatter.match({
+  "location": "SiteBarriga.queEstouAcessandoAAplicação()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "informo o usuário \"algumacoisa@gmail.com\"",
+  "keyword": "Quando "
+});
+formatter.match({
+  "location": "SiteBarriga.informoOUsuário(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "a senha \"123456789\"",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.aSenha(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono entrar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoEntrar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "visualizo a página inicial",
+  "keyword": "Então "
+});
+formatter.match({
+  "location": "SiteBarriga.visualizoAPáginaInicial()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono Contas",
+  "keyword": "Quando "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoContas()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono Adicionar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoAdicionar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Não deve inserir uma conta com nome já existente",
+  "description": "",
+  "keyword": "Cenário"
+});
+formatter.step({
+  "name": "informo a conta \"Conta de Teste\"",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.informoAConta(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "seleciono Salvar",
+  "keyword": "E "
+});
+formatter.match({
+  "location": "SiteBarriga.selecionoSalvar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "sou notificado que já existe uma conta com esse nome",
+  "keyword": "Então "
+});
+formatter.match({
+  "location": "SiteBarriga.souNotificadoQueJáExisteUmaContaComEsseNome()"
 });
 formatter.result({
   "status": "passed"
